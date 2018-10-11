8159be9134e2ae907543638277cce09bd5eddbe3
Brian Heylin <brian.heylin@gmail.com>
2018-10-11 22:29:04 +0200

    focus works for column or row layout of size 2 or 3

---
+++ FocusGroup.py
@@ -0,0 +1,42 @@
+import sublime
+import sublime_plugin
+
+
+class FocusGroupEventListener(sublime_plugin.EventListener):
+    def on_activated(self, view):
+        window  = view.window()
+        # self.settings = sublime.load_settings("FocusGroup.sublime-settings")
+
+        if window.num_groups() > 1: 
+            window.run_command("fg_focus_group")
+
+
+class FgFocusGroup(sublime_plugin.WindowCommand):
+
+    def apply_layout(self, division_count, focused_size, dimension_name, layout, active_group):
+        if division_count == 2:
+            layout[dimension_name][1] = focused_size if active_group == 0 else 1 - focused_size
+        elif division_count == 3:
+            unfocused_size = (1 - focused_size) * 0.5
+            layout[dimension_name][1] = focused_size if active_group == 0 else unfocused_size
+            layout[dimension_name][2] = layout[dimension_name][1] + (focused_size if active_group == 1 else unfocused_size)
+
+    def run(self, **args):
+        settings = sublime.load_settings("FocusGroup.sublime-settings")
+
+        layout  = self.window.get_layout()
+        view = self.window.active_view()
+        active_group = self.window.active_group()
+
+        column_count = len(layout['cols']) - 1
+        row_count = len(layout['rows']) - 1
+
+        if column_count > 1:
+            col_focused_size = settings.get('fg.focusd_size_{}_segments'.format(column_count))
+            self.apply_layout(column_count, col_focused_size, 'cols', layout, active_group)
+
+        if row_count > 1:
+            row_focused_size = settings.get('fg.focusd_size_{}_segments'.format(row_count))
+            self.apply_layout(row_count, row_focused_size, 'rows', layout, active_group)
+
+        self.window.run_command('set_layout', layout)
